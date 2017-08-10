743e280700e6f29362f098adf11cfe5b76fef9b5
Claire de Vries <cdevries@comcast.net>
2017-08-10 13:21:03 +0000

    Finished the intro lab

---
+++ methods.rb
@@ -0,0 +1,13 @@
+# # NICO-Name, Input, Code, Output
+# def throw(color, person)
+#   return "Yay, #{person} made the #{color} marker into the basket!"  
+# end
+
+# puts throw("Red", "Marley")
+# # puts throw("Blue", "Taylor")
+#  def buyTech( fcost, mcost)
+#      return fcost/mcost
+#  end
+#  puts buyTech(60000000,1000)
+ 
+ 
